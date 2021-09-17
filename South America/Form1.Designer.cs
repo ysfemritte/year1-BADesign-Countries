e1b8a9c355de4fa33b357d6b091ef9abb9b7538d
Yusuf Emritte <34180742+ysfemritte@users.noreply.github.com>
2021-09-17 17:42:25 +0400

    Add files via upload

---
+++ South America/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace South_America
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.countriesListBox = new System.Windows.Forms.ListBox();
+            this.getCountriesButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Location = new System.Drawing.Point(39, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "The Countries of South America:";
+            // 
+            // countriesListBox
+            // 
+            this.countriesListBox.FormattingEnabled = true;
+            this.countriesListBox.ItemHeight = 16;
+            this.countriesListBox.Location = new System.Drawing.Point(50, 79);
+            this.countriesListBox.Name = "countriesListBox";
+            this.countriesListBox.Size = new System.Drawing.Size(239, 148);
+            this.countriesListBox.TabIndex = 1;
+            // 
+            // getCountriesButton
+            // 
+            this.getCountriesButton.Location = new System.Drawing.Point(50, 275);
+            this.getCountriesButton.Name = "getCountriesButton";
+            this.getCountriesButton.Size = new System.Drawing.Size(102, 47);
+            this.getCountriesButton.TabIndex = 2;
+            this.getCountriesButton.Text = "Get Countries";
+            this.getCountriesButton.UseVisualStyleBackColor = true;
+            this.getCountriesButton.Click += new System.EventHandler(this.getCountriesButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(187, 275);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(102, 47);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 351);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.getCountriesButton);
+            this.Controls.Add(this.countriesListBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox countriesListBox;
+        private System.Windows.Forms.Button getCountriesButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
